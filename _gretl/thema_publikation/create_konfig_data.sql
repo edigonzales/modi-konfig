21aa64a179093acfa7325666619a8ae57f90b5f0
Stefan Ziegler <edi.gonzales@gmail.com>
2025-02-08 21:05:46 +0100

    start with gh action

--- _gretl/thema_publikation/create_konfig_data.sql
+++ _gretl/thema_publikation/create_konfig_data.sql
@@ -20,7 +20,77 @@ SELECT
     m.datenherr,
     p.publiziertAm AS publiziertAm
 FROM
+    -- Wo, welcher Pfad verwendet werden muss, wird bisschen mühsam.
+    -- Vgl. build.gradle mit den Modelldateien.
     read_json('*/meta/meta.json') AS m
     LEFT JOIN read_json('s3://ch.so.agi.fubar1/*/publishedat.json') AS p
     ON m.ID = p.ID
+;
+
+DELETE FROM
+    konfig.themapublikation_themapublikation
+;
+INSERT INTO 
+    konfig.themapublikation_themapublikation
+    (
+        T_Id,
+        T_Ili_Tid,
+        id,
+        titel,
+        beschreibung,
+        modell,
+        tags,
+        publiziertam
+    )
+SELECT 
+    T_Id,
+    T_Ili_Tid,
+    id,
+    titel,
+    beschreibung,
+    modell,
+    list_aggregate(tags, 'string_agg', ',') AS tags,
+    publiziertam
+FROM 
+    themapublikation_tmp 
+;
+
+DELETE FROM 
+    konfig.amt_
+;
+INSERT INTO 
+    konfig.amt_
+    (
+        T_Id,
+        --T_Ili_Tid,
+        aname,
+        abkuerzung,
+        abteilung,
+        amtimweb,
+        zeile1,
+        zeile2,
+        strasse,
+        hausnr,
+        plz,
+        ort,
+        thempblktn_thmpblktion_datenherr
+    )
+SELECT 
+    nextval('konfig.t_ili2db_seq') AS T_Id,
+    --T_Ili_Tid,
+    aname,
+    abkuerzung,
+    abteilung,
+    amtimweb,
+    zeile1,
+    zeile2,
+    strasse,
+    hausnr,
+    plz,
+    ort,
+    themapublikation_tmp.T_Id AS thempblktn_thmpblktion_datenherr
+FROM 
+    themapublikation_tmp
+    LEFT JOIN stammdaten.amt_amt AS amt 
+    ON themapublikation_tmp.datenherr = amt.T_Ili_Tid 
 ;
